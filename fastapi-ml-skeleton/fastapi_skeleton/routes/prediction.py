4a50d33b8adefc80cf68c40fde6c0f484f0c4854
vadimf18 <g7bwqpjccm@privaterelay.appleid.com>
2026-02-03 08:31:51 -0800

    Create prediction.py

---
+++ fastapi-ml-skeleton/fastapi_skeleton/routes/prediction.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Depends
+from starlette.requests import Request
+
+from fastapi_skeleton.core import security
+from fastapi_skeleton.models.payload import HousePredictionPayload
+from fastapi_skeleton.models.prediction import HousePredictionResult
+from fastapi_skeleton.services.models import HousePriceModel
+
+router = APIRouter()
+
+
+@router.post("/predict", response_model=HousePredictionResult, name="predict")
+def post_predict(
+    request: Request,
+    block_data: HousePredictionPayload,
+    _: bool = Depends(security.validate_request),
+) -> HousePredictionResult:
+    model: HousePriceModel = request.app.state.model
+    prediction: HousePredictionResult = model.predict(block_data)
+
+    return prediction
